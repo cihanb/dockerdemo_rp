839003116cc156b62afc0e8bf8be843043a1493c
Cihan B <Cihan B>
2017-05-02 13:48:02 -0700

    fixed scripts

--- WinScripts/create_cluster.sh
+++ WinScripts/create_cluster.sh
@@ -5,10 +5,10 @@ source ../settings.sh
 
 #create
 echo ""
-echo $info_color"INFO"$no_color": Starting Redis Enterprise Pack containers on a single network"
+echo "INFO: Starting Redis Enterprise Pack containers on a single network"
 
 echo ""
-echo $info_color"INFO"$no_color": Creating network "$rp_network_name
+echo "INFO: Creating network "$rp_network_name
 docker network create --ip-range 10.0.0.0/16 --subnet=10.0.0.0/16 $rp_network_name
 
 i=0
@@ -16,36 +16,43 @@ for ((i = 1; i<=$rp_total_nodes; i++))
 do
     if [ $i -eq 1 ]
     then 
-        #start admin ports from 8443 and +1 per node
-        rp_admin_ui_port=8443
         
         echo ""
-        echo $info_color"INFO"$no_color": Initializing the cluster with node#"$i
-        docker run -d --cap-add sys_resource --network $rp_network_name --name rp$i -p $rp_admin_ui_port:8443 redislabs/redis:latest
+        echo "INFO: Starting container for node#"$i
+        docker run -d --cpus $rp_container_cpus -m $rp_container_ram --cap-add sys_resource --network $rp_network_name --name $rp_container_name_prefix$i -p $rp_admin_ui_port:$rp_admin_ui_port -p $rp_admin_restapi_port:$rp_admin_restapi_port redislabs/redis:latest
 
         #wait for the container to launch and redis enterprise to start
-        echo $info_color"INFO"$no_color": Waiting for containers to launch and services to start"
+        echo "INFO: Waiting for containers to launch and services to start"
         sleep 30 
 
-        echo $info_color"INFO"$no_color": Initializing the cluster with node#"$i
-        winpty docker exec -d --privileged rp$i "/opt/redislabs/bin/rladmin" cluster create name $rp_fqdn username $rp_admin_account_name password $rp_admin_account_password flash_enabled
+        echo "INFO: Initializing the cluster with node#"$i
+        winpty docker exec -d --privileged $rp_container_name_prefix$i "//opt/redislabs/bin/rladmin" cluster create name $rp_fqdn username $rp_admin_account_name password $rp_admin_account_password flash_enabled
     else
         #added nodes
-        rp_admin_ui_port=$(( 8443+$i-1 ))
+        rp_admin_ui_port_mapped=$(( $rp_admin_ui_port+$i-1 ))
+        rp_admin_restapi_port_mapped=$(( $rp_admin_restapi_port+$i-1 ))
 
         echo ""
-        echo $info_color"INFO"$no_color": Initializing the cluster with node#"$i
-        docker run -d --cap-add sys_resource --network $rp_network_name --name rp$i -p $rp_admin_ui_port:8443 redislabs/redis:latest
+        echo "INFO: Starting container for node#"$i
+        docker run -d --cpus $rp_container_cpus -m $rp_container_ram --cap-add sys_resource --network $rp_network_name --name $rp_container_name_prefix$i -p $rp_admin_ui_port_mapped:$rp_admin_ui_port -p $rp_admin_restapi_port_mapped:$rp_admin_restapi_port redislabs/redis:latest
 
         
         # wait for cluster setup to finish
         sleep 30
-        echo $info_color"INFO"$no_color": Joining node#"$i" to the cluster"
-        winpty docker exec -d --privileged rp$i "/opt/redislabs/bin/rladmin" cluster join username $rp_admin_account_name password $rp_admin_account_password nodes 10.0.0.2 flash_enabled
+        echo "INFO: Joining node#"$i" to the cluster"
+        winpty docker exec -d --privileged $rp_container_name_prefix$i "//opt/redislabs/bin/rladmin" cluster join username $rp_admin_account_name password $rp_admin_account_password nodes 10.0.0.2 flash_enabled
     fi
  done
 
+#create database
+echo "INFO: Creating database sample-db on port 12000"
+curl -k -u "$rp_admin_account_name:$rp_admin_account_password" --request POST --url https://localhost:9443/v1/bdbs --request POST --url https://localhost:9443/v1/bdbs  --header 'content-type: application/json' --data '{"name":"sample-db","type":"redis","memory_size":1073741824,"port":12000}'
+
 echo ""
-echo $info_color"INFO"$no_color": "$rp_total_nodes" node Redis Enterprise Pack cluster created."
-echo $info_color"INFO"$no_color": Visit: https://localhost:8443 and create a database."
-echo $info_color"INFO"$no_color": Run: docker exec -it rp1 bash to connect to first node."
+echo "INFO: "$rp_total_nodes" node Redis Enterprise Pack cluster created."
+echo "INFO: running container status:"
+docker ps -a | grep $rp_container_name_prefix
+echo ""
+echo "INFO: NEXT STEPS:"
+echo "INFO: Visit: https://localhost:8443 to view the database and stats."
+echo "INFO: Run: docker exec -it "$rp_container_name_prefix"1 bash to connect to first node"
--- WinScripts/delete_cluster.sh
+++ WinScripts/delete_cluster.sh
@@ -4,25 +4,25 @@
 source ../settings.sh
 
 
-echo $warning_color"WARNING"$no_color": This will wipe out your cluster nodes and delete all your data on containers [y/n]"
+echo "WARNING: This will wipe out your cluster nodes and delete all your data on containers [y/n]"
 read yes_no
 
 if [ $yes_no == 'y' ]
 then
     #remove
-    echo $info_color"INFO"$no_color": Running cleanup..."
+    echo "INFO: Running cleanup..."
 
     i=0
     for ((i = 1; i<=$rp_total_nodes; i++))
     do
-        echo $info_color"INFO"$no_color": Deleting containers rp"$i
+        echo "INFO: Deleting containers rp"$i
         docker rm -f rp$i 
     done
 
-    echo $info_color"INFO"$no_color": Deleting network "$rp_network_name
+    echo "INFO: Deleting network "$rp_network_name
     docker network rm $rp_network_name    
 else
-    echo $info_color"INFO"$no_color": Cleanup Cancelled"
+    echo "INFO: Cleanup Cancelled"
 fi
 
-echo $info_color"INFO"$no_color": Done with cleanup."
+echo "INFO: Done with cleanup."
